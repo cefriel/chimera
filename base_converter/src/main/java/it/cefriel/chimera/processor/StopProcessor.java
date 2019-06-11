ecde0818dfcfeba3cc47093886daada5eae53b28
Alessio Carenini <alessio.carenini@cefriel.com>
2019-06-11 16:33:44 +0200

    Print the stack trace if an exception is detected

--- base_converter/src/main/java/it/cefriel/chimera/processor/StopProcessor.java
+++ base_converter/src/main/java/it/cefriel/chimera/processor/StopProcessor.java
@@ -1,10 +1,16 @@
 package it.cefriel.chimera.processor;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class StopProcessor implements Processor {
+    private Logger log = LoggerFactory.getLogger(StopProcessor.class); 
 
 
     /**
@@ -24,9 +30,20 @@ public class StopProcessor implements Processor {
     public void process(Exchange exchange) throws Exception {
         CamelContext camelContext = exchange.getContext();
         // remove myself from the in flight registry so we can stop this route without trouble
+        Throwable caused = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Throwable.class);
+        if (caused !=null )
+        	log.error(stack_to_string(caused));
         camelContext.getInflightRepository().remove(exchange);
         // stop the route
         camelContext.stopRoute(routeId);
     }
-
+    
+    
+    private String stack_to_string(Throwable e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String sStackTrace = sw.toString(); // stack trace as a string
+        return sStackTrace;
+    }       
 }
