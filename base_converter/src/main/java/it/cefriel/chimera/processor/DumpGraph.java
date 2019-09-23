394a6aee746b6e09ce85bedb0b0cead8d4556170
Alessio Carenini <alessio.carenini@cefriel.com>
2019-09-23 17:02:04 +0200

    Changed behaviour

--- base_converter/src/main/java/it/cefriel/chimera/processor/DumpGraph.java
+++ base_converter/src/main/java/it/cefriel/chimera/processor/DumpGraph.java
@@ -1,7 +1,10 @@
 package it.cefriel.chimera.processor;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Message;
 import org.apache.camel.Processor;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
 import org.eclipse.rdf4j.query.QueryResults;
@@ -20,13 +23,14 @@ public class DumpGraph implements Processor {
     public void process(Exchange exchange) throws Exception {
     	Message msg=exchange.getIn();
 		Repository repo=null;
-
+		ByteArrayOutputStream outstream=new ByteArrayOutputStream();
 		repo=msg.getHeader(ProcessorConstants.CONTEXT_GRAPH, RDFGraph.class).getRepository();
 		try (RepositoryConnection con = repo.getConnection()) {
 			RepositoryResult<Statement> dump = con.getStatements(null, null, null);
 			Model dump_model = QueryResults.asModel(dump);
 
-			Rio.write(dump_model, System.out, RDFFormat.TURTLE);
+			Rio.write(dump_model, outstream, RDFFormat.TURTLE);
+			exchange.getOut().setBody(new String(outstream.toByteArray(), StandardCharsets.UTF_8));
 		}
     }
 
