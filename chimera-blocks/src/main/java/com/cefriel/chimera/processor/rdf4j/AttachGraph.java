4138be4559d3b06dc0a81f1d1d1fafc35467b82e
Mario Scrocca <mario.scrocca@gmail.com>
2020-01-10 10:41:12 +0100

    Move and update Attach and Dump graph wrt RDFGraph interface

--- chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/AttachGraph.java
+++ chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/AttachGraph.java
@@ -13,22 +13,49 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cefriel.chimera.processor;
+package com.cefriel.chimera.processor.rdf4j;
 
+import com.cefriel.chimera.context.HTTPRDFGraph;
+import com.cefriel.chimera.context.RDFGraph;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
 import com.cefriel.chimera.context.MemoryRDFGraph;
 import com.cefriel.chimera.util.ProcessorConstants;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 public class AttachGraph implements Processor {
 
-	
-    public void process(Exchange exchange) throws Exception {
-    	MemoryRDFGraph graph=new MemoryRDFGraph();
-    	exchange.setProperty(ProcessorConstants.CONTEXT_GRAPH, graph);
+    private Logger log = LoggerFactory.getLogger(AttachGraph.class);
 
+    private String DB_ADDRESS;
+    private String REPOSITORY_ID;
+
+    public void process(Exchange exchange) throws Exception {
+        RDFGraph graph;
+        if (DB_ADDRESS != null && REPOSITORY_ID != null)
+            graph = new HTTPRDFGraph(DB_ADDRESS, REPOSITORY_ID);
+        else
+            graph = new MemoryRDFGraph();
+    	exchange.setProperty(ProcessorConstants.CONTEXT_GRAPH, graph);
+    }
+
+    public String getDB_ADDRESS() {
+        return DB_ADDRESS;
+    }
+
+    public void setDB_ADDRESS(String DB_ADDRESS) {
+        this.DB_ADDRESS = DB_ADDRESS;
+    }
+
+    public String getREPOSITORY_ID() {
+        return REPOSITORY_ID;
+    }
+
+    public void setREPOSITORY_ID(String REPOSITORY_ID) {
+        this.REPOSITORY_ID = REPOSITORY_ID;
     }
 
 }
--- chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/DumpGraph.java
+++ chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/DumpGraph.java
@@ -13,14 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cefriel.chimera.processor;
+package com.cefriel.chimera.processor.rdf4j;
 import java.nio.charset.StandardCharsets;
 
+import com.cefriel.chimera.context.RDFGraph;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Statement;
+import org.eclipse.rdf4j.model.ValueFactory;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.query.QueryResults;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
@@ -30,25 +34,35 @@ import org.eclipse.rdf4j.rio.Rio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.cefriel.chimera.context.MemoryRDFGraph;
 import com.cefriel.chimera.util.ProcessorConstants;
 
 
 public class DumpGraph implements Processor {
-    private Logger log = LoggerFactory.getLogger(DumpGraph.class); 
+
+    private Logger logger = LoggerFactory.getLogger(DumpGraph.class);
 
     public void process(Exchange exchange) throws Exception {
-		Repository repo=null;
-		String  output=null;
-		ByteArrayOutputStream outstream=new ByteArrayOutputStream();
-		repo=exchange.getProperty(ProcessorConstants.CONTEXT_GRAPH, MemoryRDFGraph.class).getRepository();
+		Repository repo;
+		String output;
+		ByteArrayOutputStream outstream = new ByteArrayOutputStream();
+
+		repo = exchange.getProperty(ProcessorConstants.CONTEXT_GRAPH, RDFGraph.class).getRepository();
+
+		String context = ProcessorConstants.BASE_CONVERSION_IRI
+				+ exchange.getProperty(ProcessorConstants.CONTEXT_ID, String.class);
+		ValueFactory vf = SimpleValueFactory.getInstance();
+		IRI contextIRI = vf.createIRI(context);
+
+        logger.info("CONTEXT " + context);
+        logger.info("MESSAGE_ID " + exchange.getMessage().getMessageId());
+
 		try (RepositoryConnection con = repo.getConnection()) {
-			RepositoryResult<Statement> dump = con.getStatements(null, null, null);
+			RepositoryResult<Statement> dump = con.getStatements(null, null, null, contextIRI);
 			Model dump_model = QueryResults.asModel(dump);
 
 			Rio.write(dump_model, outstream, RDFFormat.TURTLE);
-			output=new String(outstream.toByteArray(), StandardCharsets.UTF_8);
-			log.info("RDF OUTPUT\r\r"+output);
+			output = new String(outstream.toByteArray(), StandardCharsets.UTF_8);
+
 			exchange.getOut().setBody(output);
 			exchange.getOut().setHeader(Exchange.CONTENT_TYPE, "text/turtle");
 		}
