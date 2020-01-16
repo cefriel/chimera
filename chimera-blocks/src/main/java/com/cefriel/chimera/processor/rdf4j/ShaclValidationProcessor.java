460e44ea3407dfa2211ef56e32b2347126a6e10c
Mario Scrocca <mario.scrocca@gmail.com>
2020-01-16 15:06:59 +0100

    Move ShaclValidationProcessor

--- chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/ShaclValidationProcessor.java
+++ chimera-blocks/src/main/java/com/cefriel/chimera/processor/rdf4j/ShaclValidationProcessor.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cefriel.chimera.processor;
+package com.cefriel.chimera.processor.rdf4j;
 
 import java.util.List;
 
