45fa48ac4d0b443d84c172a6d898ce378539f06e
Mario Scrocca <mario.scrocca@gmail.com>
2020-01-30 12:24:50 +0100

    Add first draft chimera-example

---
+++ chimera-example/src/main/java/ConverterApplication.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2020 Cefriel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.apache.camel.CamelContext;
+import org.apache.camel.spring.SpringCamelContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class ConverterApplication {
+
+	public static void main(String[] args) throws Exception {
+		ApplicationContext appContext = new FileSystemXmlApplicationContext("./routes/camel-context.xml");
+		CamelContext camelContext = new SpringCamelContext(appContext);
+		camelContext.start();
+	}
+
+}
