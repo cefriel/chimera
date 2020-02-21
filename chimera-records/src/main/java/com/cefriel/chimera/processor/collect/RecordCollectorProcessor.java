0db1ab021aa7dd8a513accdb3c80b8bd295c0a18
Mario Scrocca <mario.scrocca@gmail.com>
2020-02-21 17:04:49 +0100

    Add chimera-records module

---
+++ chimera-records/src/main/java/com/cefriel/chimera/processor/collect/RecordCollectorProcessor.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 Cefriel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.cefriel.chimera.processor.collect;
+
+import com.cefriel.chimera.util.RecordCollector;
+import com.cefriel.chimera.util.RecordProcessorConstants;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RecordCollectorProcessor implements Processor {
+
+    private Logger logger = LoggerFactory.getLogger(RecordCollectorProcessor.class);
+
+    private String filepath = "./records.txt";
+    private int bufferSize = -1;
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String collectorId = exchange.getMessage().getHeader(RecordProcessorConstants.COLLECTOR_ID, String.class);
+        if (collectorId == null) {
+            logger.info("Collector ID not found. Attach it to header using as key " + RecordProcessorConstants.COLLECTOR_ID);
+            return;
+        }
+
+        RecordCollector collector = exchange.getProperty(collectorId, RecordCollector.class);
+        if (collector == null) {
+            String filepath;
+            int bufferSize;
+            filepath = exchange.getMessage().getHeader(RecordProcessorConstants.COLLECTOR_FILEPATH, String.class);
+            bufferSize = exchange.getMessage().getHeader(RecordProcessorConstants.COLLECTOR_BUFFER_SIZE, Integer.class);
+
+            if (filepath == null)
+                filepath = this.filepath;
+            if (bufferSize == 0)
+                bufferSize = this.bufferSize;
+
+            collector = new RecordCollector(filepath, bufferSize);
+        } else
+            collector.saveData(true);
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public int getBufferSize() {
+        return bufferSize;
+    }
+
+    public void setBufferSize(int bufferSize) {
+        this.bufferSize = bufferSize;
+    }
+}
---
+++ chimera-records/src/main/java/com/cefriel/chimera/processor/collect/TimestampCollectorProcessor.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2020 Cefriel.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package com.cefriel.chimera.processor.collect;
+
+import com.cefriel.chimera.util.RecordCollector;
+import com.cefriel.chimera.util.RecordProcessorConstants;
+import com.cefriel.chimera.util.Utils;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+public class TimestampCollectorProcessor implements Processor {
+
+    private Logger logger = LoggerFactory.getLogger(RecordCollectorProcessor.class);
+
+    private String timestampLabel = "default_label";
+    private String collectorId;
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        String collectorId = exchange.getMessage().getHeader(RecordProcessorConstants.COLLECTOR_ID, String.class);
+        if (collectorId == null)
+            collectorId = this.collectorId;
+        if (collectorId == null) {
+            logger.info("Collector ID not found. Attach it to header using as key " + RecordProcessorConstants.COLLECTOR_ID);
+            return;
+        }
+
+        String timestampLabel;
+        timestampLabel = exchange.getMessage().getHeader(RecordProcessorConstants.TIMESTAMP_LABEL, String.class);
+        if (timestampLabel == null)
+            timestampLabel = this.timestampLabel;
+
+        RecordCollector collector = exchange.getProperty(collectorId, RecordCollector.class);
+        if (collector == null) {
+            RecordCollectorProcessor rcp = new RecordCollectorProcessor();
+            rcp.process(exchange);
+        } else {
+            String[] record = new String[3];
+            record[0] = Utils.getContext(exchange);
+            record[1] = timestampLabel;
+            record[2] = Long.toString(Instant.now().toEpochMilli());
+            collector.addRecord(record);
+        }
+    }
+
+    public String getTimestampLabel() {
+        return timestampLabel;
+    }
+
+    public void setTimestampLabel(String timestampLabel) {
+        this.timestampLabel = timestampLabel;
+    }
+
+    public String getCollectorId() {
+        return collectorId;
+    }
+
+    public void setCollectorId(String collectorId) {
+        this.collectorId = collectorId;
+    }
+
+}
