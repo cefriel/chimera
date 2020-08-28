adcc277bdc228b86db94690899fe008229ad9d45
Mario Scrocca <mario.scrocca@gmail.com>
2020-08-28 11:40:22 +0200

    Update UniLoader.java

--- chimera-core/src/main/java/com/cefriel/chimera/util/UniLoader.java
+++ chimera-core/src/main/java/com/cefriel/chimera/util/UniLoader.java
@@ -26,7 +26,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class UniLoader {
-    private static Logger log = LoggerFactory.getLogger(UniLoader.class);
+    private static Logger logger = LoggerFactory.getLogger(UniLoader.class);
 
     public static InputStream open(String resource) throws MalformedURLException, IOException {
         return open(resource, null);
@@ -35,16 +35,14 @@ public class UniLoader {
     public static InputStream open(String resource, String token) throws MalformedURLException, IOException {
     	String res;
         InputStream is;
-        log.warn("Loading resource " + resource);
+        logger.info("Loading resource " + resource);
         if (resource.startsWith("classpath://")) {
         	res = resource.replace("classpath://", "");
-            log.warn("Loading classpath resource "+res);
             is = UniLoader.class.getClassLoader().getResourceAsStream(res);
             return is;
         }
         if (resource.startsWith("file://")) {
         	res = resource.replace("file://", "");
-            log.warn("Loading file resource " + res);
             is = new FileInputStream(res);
             return is;
         }
@@ -57,11 +55,10 @@ public class UniLoader {
         if (token != null)
             con.setRequestProperty("Authorization", "Bearer " + token);
         // Pull the information back from the URL
-
         try {
             is = con.getInputStream();
         } catch (Exception e) {
-            log.warn("Connection failed. Resource: " + resource);
+            logger.warn("Connection failed. Resource: " + resource);
             return null;
         }
 
