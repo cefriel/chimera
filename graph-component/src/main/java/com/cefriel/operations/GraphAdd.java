022665a0cebc5c561bf45798a6cfd1c3e4508226
Marco Grassi <grassi@cefriel.it>
2022-11-17 14:34:29 +0100

    refactoring of GraphAdd operation

--- graph-component/src/main/java/com/cefriel/operations/GraphAdd.java
+++ graph-component/src/main/java/com/cefriel/operations/GraphAdd.java
@@ -16,18 +16,44 @@
 
 package com.cefriel.operations;
 
+import com.cefriel.component.GraphBean;
 import com.cefriel.graph.RDFGraph;
+import com.cefriel.util.ChimeraConstants;
+import com.cefriel.util.StreamParser;
+import com.cefriel.util.UniLoader;
+import org.apache.camel.Exchange;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Namespace;
 import org.eclipse.rdf4j.repository.Repository;
 import org.eclipse.rdf4j.repository.RepositoryConnection;
+import org.eclipse.rdf4j.rio.Rio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class GraphAdd {
 
+    // needs jwt token, rdf format, resources (ontologyUrls),
+    // contentType (MimeType) set to outoging exchange
+    private record GraphAddHeaderParams(String rdfFormat) {}
+    private record GraphAddParams(String rdfFormat,
+                                  List<String> ontologyPaths) {}
+
+    private static GraphAddHeaderParams exchangeToGraphAddHeaderParams(Exchange e) {
+        return new GraphAddHeaderParams(e.getMessage().getHeader(ChimeraConstants.RDF_FORMAT, String.class));
+    }
+    private static GraphAddParams configToGraphAddParams(GraphBean config) {
+        return new GraphAddParams(config.getRdfFormat(), config.getResources());
+    }
+
+    private static GraphAddParams mergeHeaderParams(GraphAddHeaderParams headerParams, GraphAddParams params) {
+        return new GraphAddParams(
+                headerParams.rdfFormat() != null ? headerParams.rdfFormat() : params.rdfFormat(),
+                params.ontologyPaths());
+    }
     private static final Logger LOG = LoggerFactory.getLogger(GraphAdd.class);
 
     public static void graphAdd(RDFGraph graph, Model model) throws IOException {
@@ -39,6 +65,26 @@ public class GraphAdd {
             }
         }
         LOG.info(model.size() + " triples added to the graph");
+    }
+
+    // todo this method might be refactored to utils
+    public static Model parseOntology(Exchange exchange, String ontologyUrl, String rdfFormat, String jwtToken) throws IOException {
+        InputStream inputStream = UniLoader.open(ontologyUrl, jwtToken);
+        LOG.info("InputStream loaded from path " + ontologyUrl);
+        Rio.getParserFormatForFileName(ontologyUrl)
+                .ifPresent(format -> exchange.getMessage().setHeader(Exchange.CONTENT_TYPE, format.getMIMETypes()));
+        return StreamParser.parse(inputStream, exchange);
+    }
+
+    public static void graphAdd(RDFGraph graph, Exchange exchange, GraphBean config) throws IOException {
+        GraphAddParams params = mergeHeaderParams(exchangeToGraphAddHeaderParams(exchange), configToGraphAddParams(config));
+        graphAdd(graph, exchange, params, exchange.getMessage().getHeader(ChimeraConstants.JWT_TOKEN, String.class));
+    }
+    public static void graphAdd(RDFGraph graph, Exchange exchange, GraphAddParams params, String jwtToken) throws IOException {
+        for (String ontologyUrl : params.ontologyPaths()) {
+            Model model = parseOntology(exchange, ontologyUrl, params.rdfFormat(), jwtToken);
+            graphAdd(graph, model);
+        }
     }
 
 }
